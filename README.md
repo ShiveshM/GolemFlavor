402f8b53dd892b8fd44ae5ad45eac91b5f6b3750
Shivesh Mandalia <shivesh.mandalia@outlook.com>
2020-02-28 18:39:45 +0000

    reogranise into a python package

---
+++ README.md
@@ -0,0 +1,44 @@
+# GolemFlavor
+
+GolemFlavor is a Python package for running an analysis pipeline using
+`GolemFit`.
+
+## Installation
+GolemFlavor can be installed using `pip`
+```
+pip install git+https://github.com/ShiveshM/flavour_ratio.git
+```
+This installs GolemFlavor, along with all the necessary dependencies such as
+NumPy and SciPy.
+
+GolemFlavor uses the IceCube software [`GolemFit: The HESE
+fitter`](https://github.com/IceCubeOpenSource/GolemFit) to fit with IceCube
+HESE data. Current access is limited to IceCube collaborators. A simple
+Gaussian likelihood can be used for test purposes if this requirement is
+not found.
+
+### Dependencies
+
+GolemFlavor has the following dependencies:
+* [`Python`](https://www.python.org/) 2.7
+* [`NumPy`](http://www.numpy.org/)
+* [`SciPy`](https://www.scipy.org/)
+* [`mpmath`](http://mpmath.org/)
+* [`emcee`](https://emcee.readthedocs.io/en/stable/)
+* [`PyMultiNest`](https://johannesbuchner.github.io/PyMultiNest/)
+* [`tqdm`](https://tqdm.github.io/)
+* [`Shapely`](https://shapely.readthedocs.io/en/latest/manual.html)
+* [`Matplotlib`](https://matplotlib.org/)
+* [`python-ternary`](https://github.com/marcharper/python-ternary)
+* [`GetDist`](https://getdist.readthedocs.io/en/latest/)
+
+You can use `pip` to install the above automatically.
+
+Additional dependencies:
+* [`GolemFit`](https://github.com/IceCubeOpenSource/GolemFit)
+
+## License
+
+[MIT License](LICENSE)
+
+Copyright (c) 2020 Shivesh Mandalia
---
+++ setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+from setuptools import setup, find_packages
+
+NAME = 'GolemFlavor'
+DESCRIPTION = 'GolemFlavor: A Python package for Astrophysical Flavor analysis with GolemFit'
+MAINTAINER = 'Shivesh Mandalia'
+MAINTAINER_EMAIL = 's.p.mandalia@qmul.ac.uk'
+URL = 'https://github.com/ShiveshM/flavour_ratio'
+LICENSE = 'MIT'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(path, encoding='utf-8'):
+    with io.open(path, encoding=encoding) as f:
+        content = f.read()
+    return content
+
+def get_install_requirements(path):
+    content = read(path)
+    requirements = [req for req in content.split("\n")
+                    if req != '' and not req.startswith('#')]
+    return requirements
+
+LONG_DESCRIPTION = read(os.path.join(here,'README.md'))
+
+# Want to read in package version number from __version__.py
+about = {}
+with io.open(os.path.join(here, 'golemflavor', '__version__.py'), encoding='utf-8') as f:
+    exec(f.read(), about)
+    VERSION = about['__version__']
+
+INSTALL_REQUIRES = get_install_requirements(os.path.join(here, 'requirements.txt'))
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url=URL,
+    author=MAINTAINER,
+    author_email=MAINTAINER_EMAIL,
+    license=LICENSE,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    setup_requires=['setuptools>=38.6.0'],
+    entry_points={
+        'console_scripts': [
+            'contour=scripts.contour::main',
+            'fr=scripts.fr::main',
+            'mc_texture=scripts.mc_texture::main',
+            'mc_unitary=scripts.mc_unitary::main',
+            'mc_x=scripts.mc_x::main',
+            'plot_sens=scripts.plot_sens::main',
+            'sens=scripts.sens::main'
+        ]
+    }
+)
